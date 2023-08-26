e814b849b345d1fbf891622cdca16a94798fbb6f
jerssongamboa04 <jerssongamboa350@gmail.com>
2023-08-26 21:44:31 +0200

    ultimo vercel 2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const router = require("./routes/routes");
 
 const PORT = process.env.PORT || 8000;
+
 var pg = require('pg');
 //or native libpq bindings
 //var pg = require('pg').native
